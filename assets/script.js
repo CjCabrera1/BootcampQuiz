adf52ec0217b94fa66ef3a843a475e27d55aa57d
CJ <cabrera012500@gmail.com>
2023-09-25 19:32:56 -0500

    created the structure for the project

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var timerCD = document.getElementById("countdown");
+var mainEl = document.getElementById('main');
+// An array of questions with options and answers
+const questions = [
+    {
+      question: "Which property is used to change the background color in CSS?",
+      options: ["color", "background-color", "bgcolor", "background"],
+      correctAnswer: "background-color"
+    },
+    {
+      question: "In JavaScript, what does the '.append()' method do?",
+      options: ["Appends an HTML element to the end of a selected element",
+      "Appends an HTML element to the beginning of a selected element",
+      "Adds the element to an Array",
+      "Removes the element"],
+      correctAnswer: "Appends an HTML element to the end of a selected element"
+    }
+];
+
+// need question index
+
+// timer
+function displayQuestion(){
+
+};
+
+// NEED a function to start the game
+function startQuiz(){
+
+};
+
+// NEED a function to update timer
+function timerUpdate(){
+
+};
